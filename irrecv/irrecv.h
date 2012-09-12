ec9b8d7f48353f528ac95494bf947fad1a1e09e8
Andreas Lohr <andr3as.lohr@gmail.com>
2012-09-12 20:31:22 +0200

    reduce memory usage for rc5 only

--- irrecv/irrecv.h
+++ irrecv/irrecv.h
@@ -1,6 +1,22 @@
 #ifndef IRRECV_H
 #define IRRECV_H
 
+#undef COMPILE_DECODE_NEC  
+#undef COMPILE_DECODE_SONY
+#undef COMPILE_DECODE_RC6
+#undef COMPILE_DECODE_JVC
+
+// Length of raw duration buffer
+#if defined(COMPILE_DECODE_NEC)  || \
+    defined(COMPILE_DECODE_SONY) || \
+    defined(COMPILE_DECODE_RC6)  || \
+    defined(COMPILE_DECODE_JCV)
+#define RAWBUF 68
+#else 
+// RC5 only
+#define RAWBUF 24 
+#endif
+
 // Results returned from the decoder
 typedef struct {
   int decode_type; // NEC, SONY, RC5, UNKNOWN
@@ -27,9 +43,7 @@ int irrecv_decode(decode_results *results);
 void irrecv_resume(void);
 
 // Some useful constants
-
 #define USECPERTICK 50  // microseconds per clock interrupt tick
-#define RAWBUF 68 // Length of raw duration buffer
 
 // Marks tend to be 100us too long, and spaces 100us too short
 // when received due to sensor lag.
