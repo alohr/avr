f77a3ef0d2d16f27beb4c70a507e9c66d343ea44
Andreas Lohr <andr3as.lohr@gmail.com>
2013-03-09 18:22:56 +0100

    different powerdown durations

--- lighthouse/main.c
+++ lighthouse/main.c
@@ -6,10 +6,16 @@
 #include <util/delay.h>
 
 enum {
-    DELAY = 800
+    DELAY = 800,
+
+    WD_500_MILLIS = 0,
+    WD_2_SECONDS,
+    WD_8_SECONDS,
 };
 
-void watchdog_enable(void)
+
+
+void watchdog_enable(int seconds)
 {
     cli();
     wdt_reset();
@@ -19,8 +25,21 @@ void watchdog_enable(void)
     /* set up WDT interrupt */
     WDTCR = _BV(WDCE) | _BV(WDE);
 
-    /* start watchdog timer with 2s prescaller */
-    WDTCR = _BV(WDIE) | _BV(WDP2) | _BV(WDP1) | _BV(WDP0);
+    switch (seconds) {
+    case WD_500_MILLIS:
+	/* start watchdog timer with 500ms prescaler */
+	WDTCR = _BV(WDIE) | _BV(WDP2) | _BV(WDP0);
+	break;
+    case WD_2_SECONDS:
+	/* start watchdog timer with 2s prescaler */
+	WDTCR = _BV(WDIE) | _BV(WDP2) | _BV(WDP1) | _BV(WDP0);
+	break;
+    case WD_8_SECONDS:
+	/* start watchdog timer with 8s prescaler */
+	WDTCR = _BV(WDIE) | _BV(WDP3) | _BV(WDP0);
+	break;
+    }
+
     sei();
 }
 
@@ -44,17 +63,44 @@ void powerdown(void)
     sleep_disable();
 }
 
+void setup(void)
+{
+    MCUCR |= _BV(BODS); /* disable BOD in sleep mode */
+
+    ACSR &= _BV(ACO); /* clear analog comparator output */
+    DIDR0 |= _BV(AIN1D) | _BV(AIN0D); /* disable digital input */
+
+    DDRB = _BV(PB2) | _BV(PB4); /* PB2, PB4 output */
+    PORTB &= ~(_BV(PB2) | _BV(PB4)); /* enable pull-ups */
+}
+
 int main(void)
 {
-    DDRB = _BV(PB0); /* PB0 output */
-    PORTB &= ~(_BV(PB0)); /* enable pull-ups */
+    int blink = 0;
+
+    setup();
 
     for (;;) {
-	PORTB |= _BV(PB0);
-	_delay_ms(DELAY);
-	PORTB &= ~(_BV(PB0));
+	PORTB |= _BV(PB4);
+	_delay_ms(1);
 
-	watchdog_enable();
+	if (ACSR & _BV(ACO)) {
+	    blink = 1;
+	} else {
+	    blink = 0;
+	}
+
+	PORTB &= ~(_BV(PB4));
+
+	if (blink) {
+	    PORTB |= _BV(PB2);
+	    _delay_ms(DELAY);
+	    PORTB &= ~(_BV(PB2));
+	    watchdog_enable(WD_2_SECONDS);
+	} else {
+	    watchdog_enable(WD_8_SECONDS);
+	}
+
 	powerdown();
 	watchdog_disable();
     }
