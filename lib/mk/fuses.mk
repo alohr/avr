d0d14cf38f6e375e90f00590af808ee54066f10b
Andreas Lohr <andr3as.lohr@gmail.com>
2013-03-09 18:21:33 +0100

    added atmega8 ext flags

--- lib/mk/fuses.mk
+++ lib/mk/fuses.mk
@@ -1,5 +1,6 @@
 FUSES_2313_AT1 = -U lfuse:w:0x64:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 FUSES_2313_AT8 = -U lfuse:w:0xe4:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
+FUSES_2313_EXT = -U lfuse:w:0xce:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 
 FUSES_4313_AT1 = -U lfuse:w:0x64:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 FUSES_4313_AT8 = -U lfuse:w:0xe4:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
@@ -7,3 +8,6 @@ FUSES_4313_EXT = -U lfuse:w:0xde:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 
 FUSES_85_AT1 = -U lfuse:w:0x62:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
 FUSES_85_AT8 = -U lfuse:w:0xe2:m -U hfuse:w:0xdf:m -U efuse:w:0xff:m
+
+FUSES_M8_AT1 = -U lfuse:w:0xe1:m -U hfuse:w:0xd9:m
+FUSES_M8_EXT = -U lfuse:w:0xdf:m -U hfuse:w:0xca:m
