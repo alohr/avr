a43e70c2fc2b3000ec1a7902120647c5c2ccb4f2
Andreas Lohr <andr3as.lohr@gmail.com>
2013-03-17 17:23:57 +0100

    cleanup

--- lighthouse/main.c
+++ lighthouse/main.c
@@ -9,13 +9,12 @@ enum {
     DELAY = 800,
 
     WD_500_MILLIS = 0,
+    WD_1_SECOND,
     WD_2_SECONDS,
     WD_8_SECONDS,
 };
 
-
-
-void watchdog_enable(int seconds)
+static void watchdog_enable(int seconds)
 {
     cli();
     wdt_reset();
@@ -30,6 +29,10 @@ void watchdog_enable(int seconds)
 	/* start watchdog timer with 500ms prescaler */
 	WDTCR = _BV(WDIE) | _BV(WDP2) | _BV(WDP0);
 	break;
+    case WD_1_SECOND:
+	/* start watchdog timer with 1s prescaler */
+	WDTCR = _BV(WDIE) | _BV(WDP2) | _BV(WDP1);
+	break;
     case WD_2_SECONDS:
 	/* start watchdog timer with 2s prescaler */
 	WDTCR = _BV(WDIE) | _BV(WDP2) | _BV(WDP1) | _BV(WDP0);
@@ -43,7 +46,7 @@ void watchdog_enable(int seconds)
     sei();
 }
 
-void watchdog_disable(void)
+static void watchdog_disable(void)
 {
     cli();
     wdt_reset();
@@ -54,24 +57,31 @@ void watchdog_disable(void)
     sei();
 }
 
-void powerdown(void)
+static void sleep(int mode)
 {
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    set_sleep_mode(mode);
     sleep_enable();
-
     sleep_mode();
     sleep_disable();
+}
+
+static void powerdown(void)
+{
+    sleep(SLEEP_MODE_PWR_DOWN);
 }
 
 void setup(void)
 {
     MCUCR |= _BV(BODS); /* disable BOD in sleep mode */
 
+    PRR |= _BV(PRTIM1) | _BV(PRTIM0) | _BV(PRUSI);
+    ADCSRA = 0;
+
     ACSR &= _BV(ACO); /* clear analog comparator output */
     DIDR0 |= _BV(AIN1D) | _BV(AIN0D); /* disable digital input */
 
     DDRB = _BV(PB2) | _BV(PB4); /* PB2, PB4 output */
-    PORTB &= ~(_BV(PB2) | _BV(PB4)); /* enable pull-ups */
+    PORTB = ~(_BV(PB2) | _BV(PB4)); /* enable pull-ups */
 }
 
 int main(void)
@@ -81,16 +91,12 @@ int main(void)
     setup();
 
     for (;;) {
+	/* enable transistor */
 	PORTB |= _BV(PB4);
-	_delay_ms(1);
-
-	if (ACSR & _BV(ACO)) {
-	    blink = 1;
-	} else {
-	    blink = 0;
-	}
-
+	_delay_us(500);
+	blink = ACSR & _BV(ACO); 
 	PORTB &= ~(_BV(PB4));
+	/* disable transistor */
 
 	if (blink) {
 	    PORTB |= _BV(PB2);
